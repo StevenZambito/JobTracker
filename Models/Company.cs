389fe240a4eaed61dc8bad7169c2655a40c8e73e
StevenZambito <zambito.steven@icloud.com>
2021-03-02 08:39:25 -0500

    Defined a Company model

---
+++ Models/Company.cs
@@ -0,0 +1,21 @@
+
+namespace JobTracker.Models
+{
+
+    public class Company
+    {
+        public int Id { get; set; }
+        public string Position { get; set; }
+        public string Technology { get; set; }
+        public string ContactName { get; set; }
+        public string ContactEmail { get; set; }
+        public bool Remote { get; set; }
+        public int ContactCount { get; set; }
+        public string CompanyName { get; set; }
+        public int Salary { get; set; }
+        public bool Benefits { get; set; }
+        public string CompanyLogo { get; set; }
+
+
+    }
+}
