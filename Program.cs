fc491a01053b43408c886ad13af9b9d16c3454ba
StevenZambito <zambito.steven@icloud.com>
2021-03-02 09:23:09 -0500

    Created new instance of a model

--- Program.cs
+++ Program.cs
@@ -8,7 +8,40 @@ namespace JobTracker
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Welcome to C#");
+      var context = new DatabaseContext();
+            var company = new Company
+            {
+                Position = "C# Developer",
+                Technology = "C#",
+                ContactName = "Jim Bob",
+                ContactEmail = "jimbob@example.com",
+                Remote = true,
+                ContactCount = 2,
+                CompanyName = "Developers Plus",
+                Benefits = true,
+                CompanyLogo = "https://unsplash.com/photos/bmmcfZqSjBU/download?force=true&w=640",
+                Salary = 50000,
+            };
+            context.Companies.Add(company);
+
+            var otherCompany = new Company
+            {
+                Position = "JavaScript Developer",
+                Technology = "React",
+                ContactName = "Barbara React",
+                ContactEmail = "barb@example.com",
+                Remote = true,
+                ContactCount = 10,
+                CompanyName = "Faceboooooook",
+                Benefits = false,
+                CompanyLogo = "https://unsplash.com/photos/bmmcfZqSjBU/download?force=true&w=640",
+                Salary = 95000
+            };
+            context.Companies.Add(otherCompany);
+            context.SaveChanges();
+
+            var totalSalary = context.Companies.Average(company => company.Salary);
+            Console.WriteLine(totalSalary);
 
     }
   }
